0e1601a66904fbae3d1ba824f159780170969ff9
Ahad0712 <85945961+Ahad0712@users.noreply.github.com>
2021-06-23 12:47:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,6 +16,8 @@ function draw() {
   wall=createSprite(1500,200,60,height/2);
   car.velocityX= speed;
 
+  car.collide(wall);
+  
   if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX=0;
     var deformation=0.5 * weight * speed * speed/22500;
